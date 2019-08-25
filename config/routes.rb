8357b54654e92403652e8dc4deabeca09278958a
Camille Regnault <c.regnault.pro@gmail.com>
2019-08-25 12:17:35 +0200

    Add concact model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'form', to: 'pages#form', as: '/'
-  root to: 'pages#form'
+  get 'index', to: 'pages#index', as: '/'
+  post 'contact', to: 'pages#create'
+  root to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
